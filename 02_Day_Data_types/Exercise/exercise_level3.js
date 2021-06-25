0e300839feb1c0d20ac0d4afc569232ad2fc112e
BurdenKing <lzheng20@my.bcit.ca>
2021-06-25 11:10:42 -0700

    Finish day2 exercises

--- 02_Day_Data_types/Exercise/exercise_level3.js
+++ 02_Day_Data_types/Exercise/exercise_level3.js
@@ -8,23 +8,28 @@ console.log(sentenceLove.match("love"));
 let sentenceBecause = "You cannot end a sentence with because because because is a conjunction";
 console.log((sentenceBecause.match(/because/g) || []).length);
 
-// //3
-// const sentence = '%I $am@% a %tea@cher%, &and& I lo%#ve %te@a@ching%;. The@re $is no@th@ing; &as& mo@re rewarding as educa@ting &and& @emp%o@weri@ng peo@ple. ;I found tea@ching m%o@re interesting tha@n any ot#her %jo@bs. %Do@es thi%s mo@tiv#ate yo@u to be a tea@cher!? %Th#is 30#Days&OfJavaScript &is al@so $the $resu@lt of &love& of tea&ching'
-// let afterCleanup = sentence.replace(/[^\w\s]/g, "");
-// console.log(afterCleanup);
+//3
+const sentence = '%I $am@% a %tea@cher%, &and& I lo%#ve %te@a@ching%;. The@re $is no@th@ing; &as& mo@re rewarding as educa@ting &and& @emp%o@weri@ng peo@ple. ;I found tea@ching m%o@re interesting tha@n any ot#her %jo@bs. %Do@es thi%s mo@tiv#ate yo@u to be a tea@cher!? %Th#is 30#Days&OfJavaScript &is al@so $the $resu@lt of &love& of tea&ching'
+let afterCleanup = sentence.replace(/[^\w\s]/g, "");
+console.log(afterCleanup);
 
 
-// let arr = afterCleanup.split(/\b/);
-// const map = {};
-// arr.forEach(word => {
-//     if(map.hasOwnProperty(word)){
-//         map[word]++;
-//     }
-//     else{
-//         map[word] = 1;
-//     }
-// });
-// const frequencyArr = Object.keys(map).map(key => [key, map[key]]);
-// frequencyArr.sort((a, b) => b[1] - a[1]);
+let arr = afterCleanup.split(" ");
+const map = {};
+arr.forEach(word => {
+    if(map.hasOwnProperty(word)){
+        map[word]++;
+    }
+    else{
+        map[word] = 1;
+    }
+});
+console.log(map);
 
-// console.log(frequencyArr.slice(0, 1).map(el => el[0]));
+console.log(Object.keys(map).reduce((a,b) => map[a] > map[b] ? a : b));
+
+//
+const salarySentence  = 'He earns 5000 euro from salary per month, 10000 euro annual bonus, 15000 euro online courses per month.';
+let arrNum = salarySentence.match(/\d+/g);
+
+console.log(arrNum[0] * 12 + arrNum[1] * 1 + arrNum[2] * 12);
